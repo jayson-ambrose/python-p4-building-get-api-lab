16fd7b0aba48f22c3bab2a5180b6f5e90007b858
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-04-05 11:48:27 -0600

    done

--- server/app.py
+++ server/app.py
@@ -20,19 +20,49 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = bakery.to_dict()
+        bakeries.append(bakery_dict)
+
+    response = make_response(
+        jsonify(bakeries),
+        200,
+    )
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    response = make_response(
+        jsonify(bakery.to_dict()),
+        200
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
+def baked_goods_by_price(): 
+    baked_goods = []
+    for baked_good in BakedGood.query.order_by(BakedGood.price).all():
+        baked_good_dict = baked_good.to_dict()
+        baked_goods.append(baked_good_dict)
+
+    response = make_response(
+        jsonify(baked_goods),
+        200
+    )  
+    return response
 
 @app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+def most_expensive_baked_good(): 
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    response = make_response(
+        jsonify(most_expensive.to_dict()),
+        200
+    )  
+    return response
 
 if __name__ == '__main__':
     app.run(port=555, debug=True)
